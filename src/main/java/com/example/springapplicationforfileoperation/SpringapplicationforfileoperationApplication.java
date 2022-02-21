8bb4a534bfe20216351321c5867fc48b46cac92a
vikasreddy-elixr <vikas.reddy@theelixrlabs.com>
2022-02-21 18:49:45 +0530

    initial commit

---
+++ src/main/java/com/example/springapplicationforfileoperation/SpringapplicationforfileoperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapplicationforfileoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapplicationforfileoperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringapplicationforfileoperationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springapplicationforfileoperation/SpringapplicationforfileoperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapplicationforfileoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapplicationforfileoperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
